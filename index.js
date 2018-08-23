b0a573348e194dd763250f861429e9824aa322eb
josteinmarkeset <jostein.markeset@gmail.com>
2018-08-23 09:09:44 +0100

    Changed server default port to port 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 
-const PORT = 80;
+const PORT = 8080;
 
 function getOS(agent) {
     let OS = 'unknown';
